618633d00d33e8f84f7375fc5451828ad0b41e8d
Pamela <pamelaurra2030@gmail.com>
2024-04-15 09:42:56 -0400

    comitlisto

---
+++ assets/js/index.js
@@ -0,0 +1,26 @@
+const getPosts = async () => {
+    try {
+        let url = 'https://jsonplaceholder.typicode.com/posts'
+        let response = await fetch(url);
+        console.log(response);
+        let data = await response.json();
+
+
+
+        let nuevoArreglo = data.map((objeto) => {
+            return `<ul> 
+    <li>
+    <strong>${objeto.title}</strong>
+    <span><br>${objeto.body}</span>
+    </li>
+    </ul>`
+        })
+        document.getElementById("post-data").innerHTML = nuevoArreglo.join("");
+    }
+
+    catch (error) {
+        console.log("Estamos en el catch")
+        console.log(error)
+    }
+
+}
